845466322380eb047a4ef2bd5e4652318f03a201
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-06-11 15:03:33 +0600

    print Linked List

---
+++ Module-5/print_Linked_list.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+    }
+};
+int main(){
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+
+    // cout << head->data << endl;
+    // cout << head->next->data << endl;
+    // cout << head->next->next->data << endl;
+    // cout << head->next->next->next->data << endl;
+    Node *temp = head;
+    while(temp !=NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+    temp = head;
+    while(temp !=NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+    return 0;
+}
