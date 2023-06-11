1619b85a6931ba5383e0ab026dda4e1126331f39
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-06-11 14:16:13 +0600

    Creating a Node

---
+++ Module-5/create_a_node.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+int main(){
+    Node a, b;
+    a.data = 10;
+    b.data = 20;
+
+    a.next = &b;
+    b.next = NULL;
+    cout<<a.data<<" "<<a.next->data<<endl;
+
+
+
+    return 0;
+}
