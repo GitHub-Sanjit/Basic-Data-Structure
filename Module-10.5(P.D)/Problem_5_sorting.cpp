e86e0cbebc580f7775f5c55478ad61a353158317
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-06-20 19:13:18 +0600

    Problem 5 sorting solved

---
+++ Module-10.5(P.D)/Problem_5_sorting.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+}
+void print_normal(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int data;
+    while (true)
+    {
+        cin >> data;
+        if (data == -1)
+            break;
+        insert_tail(head, tail, data);
+    }
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->data > j->data)
+            {
+                swap(i->data, j->data);
+            }
+        }
+    }
+    print_normal(head);
+    return 0;
+}
